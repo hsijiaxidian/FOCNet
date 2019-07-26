2e28b1bb757f48d270edfb455ad8dfe579b60851
Xixi Jia <hsijiaxidian@gmail.com>
2019-07-26 18:49:51 +0800

    Delete get_model_size.m

--- get_model_size.m
+++
@@ -1,8 +0,0 @@
-function modelSize = get_model_size(net)
-    paramSizes = cellfun(@size, {net.params.value}, 'UniformOutput', false) ;
-    modelSize = 0;
-    for i = 1:numel(paramSizes)
-        modelSize = modelSize + prod(paramSizes{i}) * 4;
-    end
-end
-
